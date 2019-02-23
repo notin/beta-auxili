6f29cac8074dd79ce37ccf2b9ed643ff6aa5e1fb
chadforgit <chadforgit@gmail.com>
2019-02-23 10:03:15 -0800

    first commit

--- src/test/java/com/aws/codestar/project/controller/ConversationControllerTest.java
+++ src/test/java/com/aws/codestar/project/controller/ConversationControllerTest.java
@@ -1,6 +1,7 @@
 package com.aws.codestar.project.controller;
 
 
+import com.aws.codestar.project.controller.ConversationController;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
